cb5254b3616094f5c63038e6d346f64549878577
Colin Ogoo <ogoo.colin@gmail.com>
2018-07-13 17:57:00 +0100

    subscribe to route changes; to scroll back to top on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Location, PopStateEvent } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private _lastPoppedUrl: string;
+  private _yScrollStack = [];
+
+  constructor(
+    private router: Router,
+    private location: Location
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.location
+      .subscribe((ev: PopStateEvent) => {
+        this._lastPoppedUrl = ev.url;
+      });
+    this.router.events
+      .subscribe((ev: any) => {
+        if (ev instanceof NavigationStart) {
+          if (ev.url !== this._lastPoppedUrl) {
+            this._yScrollStack.push(window.scrollY);
+          }
+        } else if (ev instanceof NavigationEnd) {
+          if (ev.url === this._lastPoppedUrl) {
+            this._lastPoppedUrl = undefined;
+            window.scrollTo(0, this._yScrollStack.pop());
+          } else {
+            window.scrollTo(0, 0);
+          }
+        }
+      });
+  }
 }
